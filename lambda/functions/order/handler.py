3f1c56961ecb3b1c02ce232f78650ac4f36bddeb
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-12-27 00:01:37 +0100

    Adding function description and link it to SQS queue.

--- lambda/functions/order/handler.py
+++ lambda/functions/order/handler.py
@@ -2,20 +2,7 @@ import json
 
 
 def order(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event,
-    }
+    print("[INFO] Event : " + str(event))
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    if event.credit_score.score:
+        print("[DEBUG] Credit score : " + event.credit_score.score)
