2df908621b36463044863a3d3a0581965837328d
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-12-27 22:47:53 +0100

    Adding more comments.

--- lambda/functions/order/handler.py
+++ lambda/functions/order/handler.py
@@ -4,6 +4,9 @@ import sys
 
 def order(event, context):
     try:
+        # Deal with JSON formatted with single quote instead of double quotes.
+        event = event.replace("\'", "\"")
+        # Convert string to JSON.
         body = json.loads(event["Records"][0]["body"])
         # If credit score superior or equal to 660, order is accepted.
         if int(body["credit_score"]["score"]) >= 660:
