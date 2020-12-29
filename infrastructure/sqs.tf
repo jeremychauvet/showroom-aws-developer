600c5d19db0d7ca215584443f0f72ef9013a06c7
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-12-29 12:22:14 +0100

    Adding book queue

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -28,3 +28,15 @@ resource "aws_sqs_queue" "order-dlq" {
 
   tags = var.tags
 }
+
+resource "aws_sqs_queue" "book" {
+  name                      = "book"
+  delay_seconds             = var.sqs_delay_seconds
+  max_message_size          = var.sqs_max_message_size
+  message_retention_seconds = var.sqs_message_retention_seconds
+
+  kms_master_key_id                 = "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = 300
+
+  tags = var.tags
+}
