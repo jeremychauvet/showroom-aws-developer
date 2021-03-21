6176be22c30b675f484dede1dfc66d95af1e0884
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-21 23:01:49 +0100

    Setup Axios.

--- src/src/main.js
+++ src/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+export const HTTP = axios.create(
+  {
+    baseURL: process.env.BACKEND_URL
+  }
+)
+
 
 createApp(App).mount('#app')
