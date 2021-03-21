f62290d8a352c6926a1032338de9da92bba867f6
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-21 23:03:41 +0100

    Adding page to display books.

---
+++ src/src/components/DisplayBooks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="getBooks">Get books</button>
+    <h1 v-if="books"></h1>
+    <p class="error" v-if="errorMessage"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayBooks',
+  data () {
+    return {
+      books: '',
+      errors: ''
+    }
+  },
+  methods: {
+    getBooks: function () {
+      axios.get('/book/display')
+        .then(response => {
+          this.books = response.data.message
+        })
+        .catch(e => {
+          this.errors = e
+        })
+    }
+  }
+}
+</script>
