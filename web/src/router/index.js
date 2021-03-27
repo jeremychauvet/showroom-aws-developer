3558692884f3d83e8ee48685a9cc2973cdc2c7cc
Jérémy Chauvet <jer.chauvet@gmail.com>
2021-03-27 13:54:41 +0100

    VueJS 3 upgrade.

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "./views/HelloWorld"
-import Home from "./views/Home"
+import HelloWorld from "../views/HelloWorld"
+import Home from "../views/Home"
 
 Vue.use(Router);
 
