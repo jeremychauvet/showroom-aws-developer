2436b82cea54ec488cd3edd45188497634133975
Jérémy Chauvet <jer.chauvet@gmail.com>
2021-03-27 15:29:05 +0100

    Improve GlobalNav rendering.

--- web/src/components/GlobalNav.vue
+++ web/src/components/GlobalNav.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="GlobalNav">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-md">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="@/assets/logo.png" alt="" width="35" height="35">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/books">Books</router-link>
-            </li>
-          </ul>
-        </div>
+          <a class="navbar-brand" href="#">
+            <img src="@/assets/logo.png" alt="" width="35" height="35">
+          </a>
+          <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navbar-collapse collapse" id="collapseNavbar">
+              <ul class="navbar-nav ms-auto">
+                  <li class="nav-item">
+                      <router-link class="nav-link" to="/">Home</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link" to="/books">Books</router-link>
+                  </li>
+              </ul>
+          </div>
       </div>
-    </nav>
+  </nav>
+
   </div>
 </template>
 
