7295f233d3981274e515313e648824f60c9448d4
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-31 18:45:52 +0200

    Fixing variable type.

--- tests/mock/insert-fake-books-in-queue/main.py
+++ tests/mock/insert-fake-books-in-queue/main.py
@@ -30,7 +30,7 @@ def insert_messages_in_queue():
                     "isbn": str(fake.isbn13()),
                     "title": str(fake.sentence(nb_words=5)),
                     "author": str(fake.name()),
-                    "stock": random.randint(0, 20),
+                    "stock": str(random.randint(0, 20)),
                 }
             }
             # Convert dictionnary in JSON.
